953495d176d8f28eae88334e2538197213246879
majunlong <majunlong@rd.tuan800.com>
2014-06-23 14:42:36 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Suncity::Application.config.secret_token = '347abb23aa2b96161c08fe49fdcc9b4eda757a83d054da2cfe36709a370b16793d39bdf68482c110a7aadb6ee31bea3b317e7604ba137296d3a044892761fea6'
